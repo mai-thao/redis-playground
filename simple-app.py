cd08c824b14d4200380d2ae1b93e4d9f405b8328
mai-thao <mthaominn@gmail.com>
2025-04-27 22:09:17 -0500

    Add logging

--- simple-app.py
+++ simple-app.py
@@ -1,8 +1,11 @@
 from flask import Flask, jsonify
-import redis
+import redis, logging
 
 # Init a Flask app with the current script name "simple-app"
 app = Flask(__name__)
+
+# Set logger to highest INFO level so can see all log messages
+logging.basicConfig(level=logging.INFO)
 
 # Connect to Redis on port 6379
 cache = redis.Redis(host='localhost', port=6379)
@@ -19,8 +22,10 @@ def get_data():
     cached_data = cache.get(CACHE_KEY)
 
     if cached_data:
+        app.logger.info('Data was served from the cache')
         return jsonify({"source": "cache", "data": cached_data.decode('utf-8')})
     else:
+        app.logger.info('Data was not cached, caching it now')
         # If not found, return default response and cache it
         cache.setex(CACHE_KEY, CACHE_TTL, DEFAULT_RESPONSE["message"])
         return jsonify({"source": "default", "data": DEFAULT_RESPONSE["message"]})
